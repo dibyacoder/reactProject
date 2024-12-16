42b9f6bb6e0e34c648ef620eaadef56be075c916
Dibyacoder <b421020@iiit-bh.ac.in>
2024-12-16 18:51:50 +0530

    Basic understanding done

---
+++ 01_basicReact/src/Chai.jsx
@@ -0,0 +1,10 @@
+// Both js and jsx will work
+// Function name must start with capital letter.
+
+function Chai(){
+    return(
+        <h2>chai in react</h2>
+    )
+}
+
+export default Chai
---
+++ 01_basicReactVite/src/chai.jsx
@@ -0,0 +1,10 @@
+// Function name should be in upper case letter
+// jsx filetype is compulsary due to we use vite.
+
+function Chai(){
+    return(
+        <h3>chai is ready</h3>
+    )
+}
+
+export default Chai
